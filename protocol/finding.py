2cc528e841da95a5f226efe129c34b0d8a273f98
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-09-09 15:42:11 +0200

    feat(finding): add Location to define error location in document

---
+++ protocol/finding.py
@@ -0,0 +1,65 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import contextlib
+import dataclasses
+import re
+
+from protocol.solution import Solution
+
+
+@dataclasses.dataclass
+class Location:
+    """The location defines the object on which the Finding belongs to.
+
+    Examples for location:
+        page                p10
+        chapter             c2     p10
+        section             sec3   p5
+        paragraph           pa5    p10
+        sentence            s10    p10
+        word                w100   p13
+        whitespace          ws17   p3
+        image               i1     p1
+    """
+    page: int = -1
+    shortcut: str = None
+    value: int = None
+
+    def raw(self):  # pylint:disable=no-self-use
+        value = self.value if self.value else ''
+        if self.shortcut == 'p':
+            return f'p{self.page}'
+        return f'{self.shortcut}{value}p{self.page}'
+
+    PATTERN = re.compile(r'((?P<shortcut>[a-z]+)(?P<value>\d+))?p(?P<page>\d+)')
+
+    @classmethod
+    def fromstr(cls, raw: str):
+        if not raw:
+            return None
+        matched = re.match(Location.PATTERN, raw)
+        if not matched:
+            return None
+
+        page, shortcut, value = int(matched['page']), 'p', None
+        with contextlib.suppress(TypeError):
+            value = int(matched['value'])
+            shortcut = matched['shortcut']
+
+        result = cls(page=page, shortcut=shortcut, value=value)
+        return result
+
+
+@dataclasses.dataclass
+class Finding:
+    index: int = -1
+    location: Location = None
+    msgid: str = None
+    solution: Solution = None
---
+++ tests/test_finding.py
@@ -0,0 +1,44 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2019 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import pytest
+
+from protocol.finding import Location
+
+
+@pytest.mark.parametrize('location, expected', [
+    ('p10', Location(page=10, shortcut='p')),
+    ('w100p13', Location(page=13, shortcut='w', value=100)),
+    ('sec3p5', Location(page=5, shortcut='sec', value=3)),
+])
+def test_finding_location_fromstr(location, expected):
+    created = Location.fromstr(location)
+    assert created == expected
+
+
+@pytest.mark.parametrize('location', [
+    Location(page=10, shortcut='p'),
+    Location(page=13, shortcut='w', value=100),
+    Location(page=5, shortcut='sec', value=3),
+])
+def test_finding_location_fromstr_raw(location):
+    raw = location.raw()
+    assert raw
+
+    parsed = Location.fromstr(raw)
+    assert parsed == location, str(parsed)
+
+
+@pytest.mark.parametrize('raw', [
+    'notworking',
+    '',
+])
+def test_finding_location_fromstr_raw_none(raw):
+    constructed = Location.fromstr(raw)
+    assert constructed is None, constructed
