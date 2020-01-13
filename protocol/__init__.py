e5cdc4a36a95fb977cbecf6e645ff798897fbb68
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-01-13 14:31:03 +0100

    0.5.4 automatically generated release

--- protocol/__init__.py
+++ protocol/__init__.py
@@ -34,6 +34,6 @@ from .solution import Solver
 from .solution import Text
 from .solution import Web
 
-__version__ = '0.5.3'
+__version__ = '0.5.4'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
