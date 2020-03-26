58c7ba970d0ea983b1095653d13103e889178f5e
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-03-26 11:29:17 +0100

    0.8.6 automatically generated release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 Every noteable change is logged here.
 
+## v0.8.6
+
 ## v0.8.5
 
 ## v0.8.4
--- protocol/__init__.py
+++ protocol/__init__.py
@@ -55,6 +55,6 @@ from protocol.solution import parse_msgid
 from protocol.solution import parse_solutions
 from protocol.utils import driver
 
-__version__ = '0.8.5'
+__version__ = '0.8.6'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
