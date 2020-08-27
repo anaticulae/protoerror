f65f57ca8303d223ca99b0e4d22c2a5700b4276e
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-08-27 18:09:31 +0200

    1.3.1 automatically generated release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 Every noteable change is logged here.
 
+## v1.3.1
+
 ## v1.3.0
 
 ### Feature
--- protocol/__init__.py
+++ protocol/__init__.py
@@ -95,6 +95,6 @@ from protocol.solution import parse_solutions
 # utils
 from protocol.utils import driver
 
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
