2073dbe462cb2eddcc6f851a5f5bc9f9622a7e48
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-01-29 18:37:47 +0100

    test(report): verify test cli invocation

---
+++ tests/report/single.py
@@ -0,0 +1,39 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import protocol
+
+SOLUTION_1230 = """\
+Titleseite enthält keinen Prüfer
+
+Fügen Sie die Namen und die akademischen Grade der Prüfer auf der
+Titelseite hinzu.
+"""
+
+S1235 = """Short Confidence
+
+Message.
+"""
+
+
+@protocol.confidence(0.9)
+def check_1230_author_matrikel_existence(linter, titlepage):
+    if titlepage is None:
+        return
+    if titlepage.examiner:
+        return
+    linter()
+
+
+def check_1235_confidence_checker():
+    pass
+
+
+def work() -> str:
+    return ''
---
+++ tests/test_report.py
@@ -0,0 +1,51 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import os
+
+import pytest
+import utilatest
+
+import protocol
+
+
+def run_report(cmd: str, capsys):
+    root = os.path.join(protocol.ROOT)
+    _, run = protocol.integrate(root=root, features='tests.report')
+    args = dict(show=cmd)
+    with pytest.raises(SystemExit):
+        run(args)
+    stdout = utilatest.stdout(capsys)
+    return stdout
+
+
+def test_report_list(capsys):
+    stdout = run_report('list', capsys)
+    assert '~1235:' in stdout
+    assert '<<single>>' in stdout
+
+
+def test_report_show_solution(capsys):
+    stdout = run_report('1235', capsys)
+    assert 'Short Confidence' in stdout
+
+
+def test_report_show_feature(capsys):
+    stdout = run_report('single', capsys)
+    assert 'Short Confidence' in stdout
+    assert '~1235:' in stdout
+
+
+def test_report_empty_cmd(capsys):
+    stdout = run_report('list', capsys)
+    empty = run_report('', capsys)
+    assert empty == stdout
+
+    wrong = run_report('wrong', capsys)
+    assert wrong != empty
